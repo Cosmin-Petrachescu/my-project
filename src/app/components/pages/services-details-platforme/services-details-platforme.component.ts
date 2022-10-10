18547900569225eed6bd542d16dfcedeaaafba20
DESKTOP-RKIM8GT\B0019 <b00190588t@gmail.com>
2022-10-10 23:14:12 +0300

    Project

---
+++ src/app/components/pages/services-details-platforme/services-details-platforme.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-services-details-platforme',
+  templateUrl: './services-details-platforme.component.html',
+  styleUrls: ['./services-details-platforme.component.scss']
+})
+export class ServicesDetailsPlatformeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
